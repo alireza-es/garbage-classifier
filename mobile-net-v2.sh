08a7553f893b4064c0b13c3a4e22d107aa94c5bf
Alireza <alireza.es@gmail.com>
2024-03-07 21:11:19 -0700

    Update script

--- mobile-net-v2.sh
+++ mobile-net-v2.sh
@@ -9,7 +9,10 @@
 #SBATCH --partition=gpu
 
 ####### Set environment variables ###############
-module load python/anaconda3-2018.12
+module load python/anaconda3-2019.10
+conda create --name py312 python=3.10  # Since Python 3.12 might not be available, 3.10 is used as an example
+conda activate py312
+
 pip install  --user -r requirements.txt
 
 wandb login 5c59fcaac06279f7c55d328b08db1a3a0a65058e
